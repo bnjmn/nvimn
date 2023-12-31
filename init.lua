13a6d86065ab0344f17f9c9d684274e78b903d2d
Benjamin Petersen <bnjmn@users.noreply.github.com>
2023-12-31 07:40:57 -0700

    Add bnjmn settings, window size/motion helpers, toggle settings easily

--- init.lua
+++ init.lua
@@ -660,12 +660,84 @@ cmp.setup({
 })
 
 -- bnjmn keymaps
---
+
+-- Window Movement Mappings - Make switching windoes easier
+vim.keymap.set('n', '<C-h>', '<C-w>h', {noremap = true})
+vim.keymap.set('n', '<C-j>', '<C-w>j', {noremap = true})
+vim.keymap.set('n', '<C-k>', '<C-w>k', {noremap = true})
+vim.keymap.set('n', '<C-l>', '<C-w>l', {noremap = true})
+
 -- Terminal mappings for easier navigation using vim.keymap.set
 vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h', {noremap = true})
 vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j', {noremap = true})
 vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k', {noremap = true})
 vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l', {noremap = true})
+
+-- Settings
+vim.o.swapfile = false
+vim.opt.listchars = "nbsp:ϟ,eol:¬,tab:▸ ,trail:•,extends:»,precedes:«"
+vim.opt.timeoutlen = 500
+vim.opt.tabpagemax = 128
+vim.opt.textwidth = 100
+
+
+-- Toggle settings
+
+local function vim_norm_opt_toggle(key, opt, on, off, desc)
+  vim.keymap.set('n', key, function()
+    local state = vim.opt[opt]:get() == off and on or off
+    vim.opt[opt] = state
+    local message = desc .. (state == on and " Enabled" or " Disabled")
+    vim.notify(message)
+  end, { desc = "toggle " .. desc })
+end
+
+vim_norm_opt_toggle('<leader>th', 'hlsearch', true, false, 'Highlight Search')
+vim_norm_opt_toggle('<leader>tl', 'list', true, false, 'List Characters')
+vim_norm_opt_toggle('<leader>tn', 'number', true, false, 'Line Numbers')
+vim_norm_opt_toggle('<leader>tp', 'paste', true, false, 'Paste Mode')
+vim_norm_opt_toggle('<leader>tw', 'wrap', true, false, 'Wrap')
+vim_norm_opt_toggle('<leader>tsp', 'spell', true, false, 'Spell Checker')
+
+-- Resize window height
+vim.keymap.set('n', '+', function()
+  vim.cmd('resize ' .. math.floor(vim.fn.winheight(0) * 10 / 9))
+end, {silent = true})
+
+vim.keymap.set('n', '-', function()
+  vim.cmd('resize ' .. math.floor(vim.fn.winheight(0) * 9 / 10))
+end, {silent = true})
+
+-- Resize window width
+vim.keymap.set('n', '<C-w>>', function()
+  vim.cmd('vertical resize ' .. math.floor(vim.fn.winwidth(0) * 10 / 9))
+end, {silent = true})
+vim.keymap.set('n', '<C-w><', function()
+  vim.cmd('vertical resize ' .. math.floor(vim.fn.winwidth(0) * 9 / 10))
+end, {silent = true})
+
+-- ColorColumn - to warn about line lengths getting out of control
+vim.wo.colorcolumn = ""
+
+-- Define the highlight for ColorColumn
+vim.cmd([[highlight ColorColumn ctermbg=NONE guibg=#335555]])
+
+local function toggle_colorcolumn()
+  if vim.wo.colorcolumn == "" then
+    -- Retrieve the textwidth dynamically from the buffer options
+    local cc_val = tostring(vim.bo.textwidth + 1)
+    vim.wo.colorcolumn = cc_val
+    vim.notify("ColorColumn Enabled at: " .. cc_val)
+  else
+    vim.wo.colorcolumn = ""
+    vim.notify("ColorColumn Disabled")
+  end
+end
+
+-- Map the toggle function to a key combination
+vim.keymap.set('n', '<leader>tc', toggle_colorcolumn, {desc = "toggle ColorColumn"})
+
+-- Testing
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
