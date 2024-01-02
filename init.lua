1c69c2c0b5ba09c93d17022bcf09cd2312f2529a
Benjamin Petersen <bnjmn@users.noreply.github.com>
2024-01-01 18:18:04 -0700

    Update intro notes

--- init.lua
+++ init.lua
@@ -1,22 +1,11 @@
 --[[
 
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
+  Initialized by Kickstart.nvim (https://github.com/nvim-lua/kickstart.nvim)
+  Customized by bnjmn (https://github.com/bnjmn)
 
   If you don't know anything about Lua, I recommend taking some time to read through
   a guide. One possible example:
   - https://learnxinyminutes.com/docs/lua/
-
 
   And then you can explore or search through `:help lua-guide`
   - https://neovim.io/doc/user/lua-guide.html
