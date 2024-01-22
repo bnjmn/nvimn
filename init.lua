1112a21dc91a8b56e9d5e3ec77b497d7bda0fd0d
Benjamin Petersen <bnjmn@users.noreply.github.com>
2024-01-22 10:42:51 -0700

    update telescope config to search hidden files

--- init.lua
+++ init.lua
@@ -311,13 +311,34 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
-require("telescope").setup({
+--
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
 	defaults = {
+		-- `hidden = true` is not supported in text grep commands.
+		vimgrep_arguments = vimgrep_arguments,
 		mappings = {
 			i = {
 				["<C-u>"] = false,
 				["<C-d>"] = false,
 			},
+		},
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
 		},
 	},
 })
@@ -555,6 +576,12 @@ local servers = {
 	-- tsserver = {},
 	-- html = { filetypes = { 'html', 'twig', 'hbs'} },
   bashls = {},
+  yamlls = {},
+  helm_ls = {
+    yamlls = {
+      path = "yaml-language-server",
+    }
+  },
 
 	lua_ls = {
 		Lua = {
